82bd81271b97054bc2ce0b00cccf6aaf228f7d05
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-02-11 02:39:48 -0500

    Create ScopeBehavior.php

---
+++ Model/Behavior/ScopeBehavior.php
@@ -0,0 +1,45 @@
+<?php
+App::uses('ModelBehavior', 'Model');
+
+class ScopeBehavior extends ModelBehavior
+{
+    public function scopedFind(Model $model, $type)
+    {
+        if (empty($model->scopes)) {
+            return array();
+        }
+
+        if (!isset($model->scopes[$type])) {
+            return array();
+        }
+
+        $config = $model->scopes[$type];
+        if ($config['find'] === null) {
+            return array();
+        }
+
+        if (!empty($config['find']['virtualFields'])) {
+            $model->virtualFields = $config['find']['virtualFields'];
+        }
+
+        if (!empty($config['find']['options']['contain'])) {
+            $model->Behaviors->attach('Containable');
+        }
+
+        return $model->find($config['find']['type'], $config['find']['options']);
+    }
+
+    public function scopes(Model $model)
+    {
+        if (empty($model->scopes)) {
+            return array();
+        }
+
+        $data = array();
+        foreach ($model->scopes as $group => $config) {
+            $data[$group] = $config['name'];
+        }
+
+        return $data;
+    }
+}
