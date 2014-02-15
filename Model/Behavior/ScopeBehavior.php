d274a321cdab5d374222df0eb2c59850ad0db359
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-02-15 16:49:59 -0500

    Add the ability to use built-in custom finds
    Instead of doing:
    
        $Post->scopedFind('top')
    
    You can now do:
    
        $Post->find('top')

--- Model/Behavior/ScopeBehavior.php
+++ Model/Behavior/ScopeBehavior.php
@@ -1,43 +1,93 @@
 <?php
+App::uses('Model', 'Model');
 App::uses('ModelBehavior', 'Model');
 
 class ScopeBehavior extends ModelBehavior {
 
-	public function scopedFind(Model $model, $type) {
-		if (empty($model->scopes)) {
-			return array();
-		}
+    protected $contains = array();
 
-		if (!isset($model->scopes[$type])) {
-			return array();
-		}
+    protected $virtualFields = array();
 
-		$config = $model->scopes[$type];
-		if ($config['find'] === null) {
-			return array();
-		}
+    public function setup(Model $model, $config = array()) {
+        foreach ($model->scopes as $type => $options) {
+            $this->mapMethods = array('/^_find' . ucfirst($type) .'$/' => '_find' . ucfirst($type));
+            $model->findMethods[$type] = true;
+        }
+    }
 
-		if (!empty($config['find']['virtualFields'])) {
-			$model->virtualFields = $config['find']['virtualFields'];
-		}
+    public function __call($method, $params) {
+        $type = lcfirst(preg_replace('/^_find/', '', $method));
+        if ($type == $method || empty($params) || !($params[0] instanceOf Model)) {
+            throw new CakeException(__d('cake_dev', 'Method %s does not exist', $method));
+        }
 
-		if (!empty($config['find']['options']['contain'])) {
-			$model->Behaviors->attach('Containable');
-		}
+        $results = isset($params[4]) ? $params[4] : array();
+        return $this->_findScoped($params[0], $type, $params[2], $params[3], $results);
+    }
 
-		return $model->find($config['find']['type'], $config['find']['options']);
-	}
+    protected function _findScoped(Model $model, $type, $state, $query, $results = array()) {
+        if (empty($model->scopes)) {
+            throw new CakeException(__d('cake_dev', 'Method %s does not exist', $method));
+        }
 
-	public function scopes(Model $model) {
-		if (empty($model->scopes)) {
-			return array();
-		}
+        if (!isset($model->scopes[$type])) {
+            throw new CakeException(__d('cake_dev', 'Method %s does not exist', $method));
+        }
 
-		$data = array();
-		foreach ($model->scopes as $group => $config) {
-			$data[$group] = $config['name'];
-		}
+        $config = $model->scopes[$type];
 
-		return $data;
-	}
+        if ($state === 'before') {
+            if (!empty($config['find']['virtualFields'])) {
+                $this->virtualFields[$model->alias] = $model->virtualFields;
+                $model->virtualFields = $config['find']['virtualFields'];
+            }
+
+            if (!empty($config['find']['options']['contain'])) {
+                $model->Behaviors->attach('Containable');
+            }
+
+            foreach ($query as $key => $value) {
+                if ($value === null && isset($config['find']['options'][$key])) {
+                    $query[$key] = $config['find']['options'][$key];
+                }
+            }
+
+            $query = Set::merge($config['find']['options'], $query);
+            $method = '_find' . ucfirst($config['find']['type']);
+            if ($method == '_findAll') {
+                return $query;
+            }
+
+            return $model->$method($state, $query, $results);
+        }
+
+        if (!empty($config['find']['virtualFields'])) {
+            $model->virtualFields = $this->virtualFields[$model->alias];
+            $this->virtualFields[$model->alias] = null;
+        }
+
+        $method = '_find' . ucfirst($config['find']['type']);
+        if ($method == '_findAll') {
+            return $results;
+        }
+
+        return $model->$method($state, $query, $results);
+    }
+
+    public function scopedFind(Model $model, $type, $options = array()) {
+        return $model->find($type, $options);
+    }
+
+    public function scopes(Model $model) {
+        if (empty($model->scopes)) {
+            return array();
+        }
+
+        $data = array();
+        foreach ($model->scopes as $group => $config) {
+            $data[$group] = $config['name'];
+        }
+
+        return $data;
+    }
 }
